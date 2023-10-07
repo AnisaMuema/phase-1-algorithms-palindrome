2700f395040e66919889f596da539a77301e1460
anisamuema <anisa.muema.school@gmail.com>
2023-10-07 14:05:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
+function isPalindrome(mom) {
+  // Write your algorithm here
+  if(mom){
+    return mom;
+  }
+  else{
+    return robot
+  }
+}
 /* 
   Add your pseudocode here
+  
+  Initalize mom as an arguement in "isPalindrome" function
+  Add noon as the condition in the IF statement
+  return mom if true
+  else return robot if false
 */
 
 /*
   Add written explanation of your solution here
+The passed arguement as string "mom" in the function "isPalindrome" is used 
+as the condition in the if statement. 
+Meaning, if the conditions are met then the output should me mom.
+Else output will be robot
+
 */
 
 // You can run `node index.js` to view these console logs
